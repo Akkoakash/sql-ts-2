b948d44c5f05ee2844580342f39bf9121fa6bb7b
Akash kumar <92292782+Akkoakash@users.noreply.github.com>
2022-05-03 13:39:43 +0530

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `GuviZenClassStudent` (
+  `Name` Akash Kumar,
+  `Email_id` akashsai1999,
+  `course` Full_Stack_Development,
+  `Location` Chennai
+);
+
+CREATE TABLE `Batch` (
+  `Batch` B30WDTamil,
+  `WeekDayStudent` Yes,
+  `WeekEndStudent` NO,
+  `Language_preference` Tamil
+);
+
+CREATE TABLE `Task` (
+  `JAVA_SCRIPT` completed,
+  `HTML` completed,
+  `CSS` completed,
+  `React_JS` completed,
+  `MYSQL` completed,
+  `MONGODB` completed,
+  `NODE_JS` completed
+);
+
+CREATE TABLE `Mentor` (
+  `JAVA_SCRIPT` SaiMohan,
+  `HTML` SaiMohan,
+   `CSS` SaiMohan,
+  `React_JS` RaghavKumar,
+  `MYSQL` RaghavKumar,
+  `MONGODB` RaghavKumar,
+  `NODE_JS` RaghavKumar
+);
